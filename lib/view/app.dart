5b4bd20bc143e925b03c01715b91992e676ccd17
Taiki-tkmr <takemurataiki.job@gmail.com>
2022-04-30 13:30:35 +0900

    add:device_preview追加

---
+++ lib/view/app.dart
@@ -0,0 +1,83 @@
+import 'package:device_preview_screenshot/device_preview_screenshot.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class QuizApp extends StatelessWidget {
+  const QuizApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //デパイス確認
+    return DevicePreview(
+      enabled: kDebugMode,
+      builder: (context) => const _App(),
+      tools: const [
+        ...DevicePreview.defaultTools,
+        // スクショ機能を追加
+        DevicePreviewScreenshot(),
+      ],
+    );
+  }
+}
+
+class _App extends StatelessWidget {
+  const _App();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
