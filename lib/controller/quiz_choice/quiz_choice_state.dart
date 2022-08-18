5888ab4382b5cf27ae9c5ea2efcb0f7493aed0b9
Taiki-tkmr <takemurataiki.job@gmail.com>
2022-08-18 19:29:40 +0900

    add

--- lib/controller/quiz_choice/quiz_choice_state.dart
+++ lib/controller/quiz_choice/quiz_choice_state.dart
@@ -1,17 +1,17 @@
-// import 'package:freezed_annotation/freezed_annotation.dart';
-//
-// import '../../entity/quiz_item.dart';
-//
-// part 'quiz_choice_screen_state.freezed.dart';
-//
-// @freezed
-// class QuizChoiceScreenState with _$QuizChoiceScreenState {
-//   QuizChoiceScreenState._();
-//   const factory QuizChoiceScreenState({
-//     @Default(false) final bool isAnsView,
-//     @Default(0) final int quizIndex, //問題番号
-//     @Default(0) final int lapIndex, //何周目
-//     @Default([]) final List<LearnQuiz> knowRememberQuestions,
-//     @Default([]) final List<LearnQuiz> unKnowRememberQuestions,
-//   }) = _Create;
-// }
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../../entity/quiz_item.dart';
+
+part 'quiz_choice_screen_state.freezed.dart';
+
+@freezed
+class QuizChoiceScreenState with _$QuizChoiceScreenState {
+  QuizChoiceScreenState._();
+  const factory QuizChoiceScreenState({
+    @Default(false) final bool isAnsView,
+    @Default(0) final int quizIndex, //問題番号
+    @Default(0) final int lapIndex, //何周目
+    @Default([]) final List<LearnQuiz> knowRememberQuestions,
+    @Default([]) final List<LearnQuiz> unKnowRememberQuestions,
+  }) = _Create;
+}
