3e79f444636e3b2c468e0bd58bc0ae5eb4fcad0b
TakemuraTaiki <taitake204@gmail.com>
2024-03-18 22:11:06 +0900

    add

--- lib/controller/admob/admob_controller.dart
+++ lib/controller/admob/admob_controller.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:state_notifier/state_notifier.dart';
@@ -39,13 +40,43 @@ class AdMobController extends StateNotifier<AdMobState> with LocatorMixin {
     super.dispose();
   }
 
+  String getAdBannerUnitId() {
+    String bannerUnitId = "";
+    if (Platform.isAndroid) {
+      // Android のとき
+      bannerUnitId = kDebugMode
+          ? 'ca-app-pub-3940256099942544/6300978111' // Androidのテスト広告ID
+          : "ca-app-pub-6280947803421936/3698614341"; // 本番
+    } else if (Platform.isIOS) {
+      // iOSのとき
+      bannerUnitId = kDebugMode
+          ? 'ca-app-pub-3940256099942544/2934735716' // iOSのテスト広告ID
+          : "ca-app-pub-6280947803421936/7070404216"; //本番
+    }
+    return bannerUnitId;
+  }
+
+  String getAdInterstitialUnitId() {
+    String bannerUnitId = "";
+    if (Platform.isAndroid) {
+      // Android のとき
+      bannerUnitId = kDebugMode
+          ? 'ca-app-pub-3940256099942544/1033173712' // Androidのテスト広告ID
+          : "ca-app-pub-6280947803421936/3267261705"; // 本番
+    } else if (Platform.isIOS) {
+      // iOSのとき
+      bannerUnitId = kDebugMode
+          ? 'ca-app-pub-3940256099942544/4411468910' // iOSのテスト広告ID
+          : "ca-app-pub-6280947803421936/6859022352"; //本番
+    }
+    return bannerUnitId;
+  }
+
   // バナー広告を新しく生成するメソッド
   Future<BannerAd> createNewBannerAd() async {
     try {
       final newBannerAd = BannerAd(
-        adUnitId: Platform.isAndroid
-            ? 'ca-app-pub-3940256099942544/6300978111' // Androidのテスト広告ID
-            : 'ca-app-pub-3940256099942544/2934735716', // iOSのテスト広告ID
+        adUnitId: getAdBannerUnitId(),
         size: AdSize.banner,
         request: const AdRequest(),
         listener: BannerAdListener(
@@ -72,9 +103,7 @@ class AdMobController extends StateNotifier<AdMobState> with LocatorMixin {
     final completer = Completer<InterstitialAd?>();
 
     InterstitialAd.load(
-      adUnitId: Platform.isAndroid
-          ? 'ca-app-pub-3940256099942544/1033173712'
-          : 'ca-app-pub-3940256099942544/4411468910',
+      adUnitId: getAdInterstitialUnitId(),
       request: const AdRequest(),
       adLoadCallback: InterstitialAdLoadCallback(
         onAdLoaded: (InterstitialAd ad) {
@@ -134,9 +163,11 @@ class AdMobController extends StateNotifier<AdMobState> with LocatorMixin {
 
 ///本番環境
 // Android:
-// バナー:ca-app-pub-5053471763020307~7242186229
+// バナー:ca-app-pub-6280947803421936/3698614341
+// インターステシャル：ca-app-pub-6280947803421936/3267261705
 // ネイティブアドバンス:ca-app-pub-5053471763020307~7242186229
 //
 //iOS:
-// バナー:ca-app-pub-5053471763020307/3260224205
-// ネイティブアドバンス；ca-app-pub-5053471763020307~1414649631
+// バナー:ca-app-pub-6280947803421936/7070404216
+// ネイティブアドバンス；ca-app-pub-6280947803421936/5518020525
+// インターステシャル：ca-app-pub-6280947803421936/6859022352
